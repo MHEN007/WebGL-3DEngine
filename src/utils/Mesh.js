5e225baa2f3386ae8dd7ed7e609ec0c3e804bc88
Agsha Athalla Nurkareem <agshaathalla@gmail.com>
2024-05-14 14:24:44 +0700

    feat: toJSON and fromJSON for mesh

--- src/utils/Mesh.js
+++ src/utils/Mesh.js
@@ -21,4 +21,28 @@ class Mesh extends NodeScene {
     setMaterial(material){
         this.material = material
     }
+
+    get type(){
+        return "Mesh"
+    }
+
+    toJSON(){
+        data = super.toJSON()
+        return {
+            ...data,
+            geometry: this.geometry.toJSON(),
+            material: this.material.toJSON(),
+            type: this.type,
+        }
+    }
+
+    static fromJSON(json, object = null){
+        if(!object){
+            object = new Mesh()
+        }
+        super.fromJSON(json, object)
+        object.geometry = BufferGeometry.fromJSON(json.geometry)
+        object.material = TexturedMaterial.fromJSON(json.material)
+        return object
+    }
 }
