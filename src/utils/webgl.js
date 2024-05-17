3232d9d767a5404d7a73373c4e1e9510bf79e4ae
Raditss <raditya.naufal.a@gmail.com>
2024-05-17 15:33:15 +0700

    feat: cam orbit

--- src/utils/webgl.js
+++ src/utils/webgl.js
@@ -4,17 +4,26 @@ const plane = new PlaneGeometry(1,1);
 const canvas = document.getElementById("glCanvas")
 const gl = canvas.getContext("webgl")
 const projectionSelector = document.getElementById("projection")
+const distanceLabel = document.getElementById("distanceLabel")
 const distanceSlider = document.getElementById("distance")
 const resetButton = document.getElementById("reset")
+const camRotationXSlider = document.getElementById("rotationX")
+const camRotationYSlider = document.getElementById("rotationY")
 const angleObliqueSlider = document.getElementById("angleOblique")
+const angleObliqueLabel = document.getElementById("angleObliqueLabel")
+const viewAngleLabel = document.getElementById("viewAngleLabel")
+const viewAngleSelector = document.getElementById("viewAngle")
 const xPos = document.getElementById("x")
 const yPos = document.getElementById("y")
 const zPos = document.getElementById("z")
+const anim = document.getElementById('anim');
 canvas.width = 600
 canvas.height = 600
 
 
 let camera = new PerspectiveCamera(45 * Math.PI / 180, canvas.width / canvas.height, 0.1, 100)
+distanceSlider.style.display = 'block'
+distanceLabel.style.display = 'block'
 camera.position = new Vector3(0, 1, 1)
 camera.rotation = new Vector3(0, 0, 0)
 
@@ -28,7 +37,7 @@ const cyan = new BasicMaterial("cyan", [0, 1, 1], camera.position)
 const materials = [green, purple, yellow, blue, cyan, red]
 
 const mesh1 = new Mesh(box, materials, [0, 1, 2, 3, 4, 5])
-mesh1.position = new Vector3(0, 0, 0)
+mesh1.position = new Vector3(-0.1, 0, 0)
 mesh1.rotation = new Vector3(0, 0, 0)
 
 
@@ -67,33 +76,70 @@ scene.drawAll()
 projectionSelector.addEventListener('change', function(){
     if (projectionSelector.value === 'perspective'){
         camera = new PerspectiveCamera(45 * Math.PI / 180, canvas.width / canvas.height, 0.1, 100)
-        distanceSlider.value = -1
+        angleObliqueLabel.style.display = 'none'
+        angleObliqueSlider.style.display = 'none'
+        distanceSlider.style.display = 'block'
+        distanceLabel.style.display = 'block'
+        distanceSlider.value = 1;
+        viewAngleLabel.style.display = 'none'
+        viewAngleSelector.style.display = 'none'
+        camera.position = new Vector3(0, 1, 1)
+        camera.rotation = new Vector3(0, 0, 0)
     }else if (projectionSelector.value === 'orthographic'){
         camera = new Orthographic(left, right, topp, bottom, near, far);
-        distanceSlider.value = -1
+        angleObliqueLabel.style.display = 'none'
+        angleObliqueSlider.style.display = 'none'
+        distanceSlider.style.display = 'none'
+        distanceLabel.style.display = 'none'
+        distanceSlider.value = 1
+        viewAngleLabel.style.display = 'block'
+        viewAngleSelector.style.display = 'block'
+        viewAngleSelector.value = 'front'
+        camera.position = new Vector3(0, 0, 1)
     }else if (projectionSelector.value === 'oblique'){
         camera = new Oblique(left, right, topp, bottom, near, far, -45);
-        distanceSlider.value = -1
+        angleObliqueLabel.style.display = 'block'
+        angleObliqueSlider.style.display = 'block'
+        distanceSlider.style.display = 'none'
+        distanceLabel.style.display = 'none'
+        distanceSlider.value = 1
+        viewAngleLabel.style.display = 'none'
+        viewAngleSelector.style.display = 'none'
+        camera.position = new Vector3(0, 0, 1)
     }
-    camera.position = new Vector3(0, 0, 1)
+    console.log(camera)
     scene.drawAll()
 
 })
 
-distanceSlider.addEventListener('input', function(){
-    console.log(distanceSlider.value)
-    if (camera.type === 'PerspectiveCamera'){
-        camera.far = parseFloat(distanceSlider.value)
-    } else if (camera.type === 'Orthographic'){
-        camera.far = parseFloat(distanceSlider.value)
-    } else if (camera.type === 'ObliqueCamera'){
-        camera.far = parseFloat(distanceSlider.value)
-    }
-
-    mesh1.rotation.y = parseFloat(distanceSlider.value)
-    mesh2.rotation.y = parseFloat(distanceSlider.value)
+camRotationYSlider.addEventListener('input', function(){
+    // unchecked anim
+    isAnimating = false;
+    anim.checked = false;
+    camera.rotation.y = parseFloat(camRotationYSlider.value)
     scene.drawAll()
+    
+})
 
+camRotationXSlider.addEventListener('input', function(){
+    camera.rotation.x = parseFloat(camRotationXSlider.value)
+    console.log(camera.rotation)
+    scene.drawAll()
+})
+
+distanceSlider.addEventListener('input', function(){
+    // console.log(distanceSlider.value)
+    // if (camera.type === 'PerspectiveCamera'){
+    //     camera.far = parseFloat(distanceSlider.value)
+    // } else if (camera.type === 'Orthographic'){
+    //     camera.far = parseFloat(distanceSlider.value)
+    // } else if (camera.type === 'ObliqueCamera'){
+    //     camera.far = parseFloat(distanceSlider.value)
+    // }
+    // console.log(camera.type)
+    camera.position.z = parseFloat(distanceSlider.value)
+    // camera.position.z = camera.position.z
+    scene.drawAll()
 })
 
 angleObliqueSlider.addEventListener('input', function(){
@@ -105,9 +151,32 @@ angleObliqueSlider.addEventListener('input', function(){
     scene.drawAll()
 })
 
+viewAngleSelector.addEventListener('change', function(){
+    if (viewAngleSelector.value === 'front'){
+        camera.position = new Vector3(0, 0, 1)
+        camera.rotation = new Vector3(0, 0, 0)
+    } else if (viewAngleSelector.value === 'back'){
+        camera.position = new Vector3(0, 0, -1)
+        camera.rotation = new Vector3(0, 0, 0)
+    // } else if (viewAngleSelector.value === 'top'){
+    //     camera.position = new Vector3(0.5, 1, 0)
+    //     camera.rotation = new Vector3(0, 0, 0)
+    // } else if (viewAngleSelector.value === 'bottom'){
+    //     camera.position = new Vector3(0, -1, 0)
+    //     camera.rotation = new Vector3(0, 0, 0)
+    } else if (viewAngleSelector.value === 'left'){
+        camera.position = new Vector3(-1, 0, 0)
+        camera.rotation = new Vector3(0, 0, 0)
+    } else if (viewAngleSelector.value === 'right'){
+        camera.position = new Vector3(1, 0, 0)
+        camera.rotation = new Vector3(0, 0, 0)
+    }
+    scene.drawAll()
+})
+
 resetButton.addEventListener('click', function(){
     camera.position = new Vector3(1, 1, 1)
-    distanceSlider.value = -1
+    // distanceSlider.value = -1
     camera.far = parseFloat(distanceSlider.value)
     camera.updateProjectionMatrix()
     scene.drawAll()
@@ -146,15 +215,15 @@ function animate() {
             rotationAngle = -3.14
         }
         // set distanceSlider value
-        distanceSlider.value = rotationAngle
-        scene.rotation.y = rotationAngle; // Update rotation of the mesh
-        scene.computeWorldMatrix(false, true);
+        camRotationYSlider.value = rotationAngle
+        camera.rotation.y = rotationAngle; // Update rotation of the mesh
+        // scene.computeWorldMatrix(false, true);
         scene.drawAll(); // Redraw the scene
     }
     requestAnimationFrame(animate); // Call animate function again in next frame
 }
 // Add event listener to the checkbox
-const anim = document.getElementById('anim');
+
 anim.addEventListener('change', function() {
     isAnimating = anim.checked; // Update animation state based on checkbox state
     if (isAnimating) {
@@ -166,3 +235,5 @@ anim.addEventListener('change', function() {
 function isPowerOf2(value) {
     return (value & (value - 1)) === 0;
 }
+
+console.log(camera);
