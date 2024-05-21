1ec7db8a9d285948ac7c3ee52b289c05d84d0ab4
haikalardzi <haikalardzi@gmail.com>
2024-05-21 23:33:52 +0700

    fix: confilct

--- src/utils/Load.js
+++ src/utils/Load.js
@@ -1,13 +1,33 @@
 /**
  * 
  * @param {Blob} file 
- * @returns
+ * @returns 
  */
-function readImage(file){
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-    const json = JSON.parse(file.text())
-    console.log(json)
-    return json
-}
+function readFile(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+  
+      reader.onload = function(event) {
+        const data = event.target.result;
+        resolve(data);
+      };
+  
+      reader.onerror = function() {
+        reject(reader.error);
+      };
+  
+      reader.readAsText(file);
+    });
+  }
 
+// fileSelector.addEventListener('change', async (e) => {
+//     const file = e.target.files[0];
+//     if (!file) return
+
+//     try{
+//         json = await readFile(file)
+//     } catch (error){
+//         console.error(error);
+//     }
+//     scene = Scene.fromJSON(json)
+// })
