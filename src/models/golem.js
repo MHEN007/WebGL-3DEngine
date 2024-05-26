fe23aecc22d6794684ed571cf80eebd474fd0b22
Raditss <raditya.naufal.a@gmail.com>
2024-05-26 11:43:23 +0700

    add: readme

--- src/models/golem.js
+++ src/models/golem.js
@@ -1,16 +1,16 @@
 class Golem{
     #headGeometry
     #torsoGeometry
-    #upperArmGeometry
-    #upperLegGeometry
+    #armGeometry
+    #legGeometry
     #noseGeometry
     #hipGeometry
 
     constructor(){
         this.#headGeometry = new BoxGeometry(0.12, 0.2, 0.1);
         this.#torsoGeometry = new BoxGeometry(0.28, 0.2, 0.1);
-        this.#upperArmGeometry = new BoxGeometry(0.07, 0.45, 0.05);
-        this.#upperLegGeometry = new BoxGeometry(0.07, 0.25, 0.1);
+        this.#armGeometry = new BoxGeometry(0.07, 0.45, 0.05);
+        this.#legGeometry = new BoxGeometry(0.07, 0.25, 0.1);
         this.#hipGeometry = new BoxGeometry(0.18, 0.12, 0.1)
         this.#noseGeometry = new BoxGeometry(0.03, 0.07, 0.02)
 
@@ -21,17 +21,17 @@ class Golem{
         this.torsoMesh = new Mesh("Torso", this.#torsoGeometry, materials, [6,6,6,6,6,6]);
         this.torsoMesh.position = new Vector3(0, 0.3, 0);
 
-        this.upperArmLeftMesh = new Mesh("Upper Arm Left", this.#upperArmGeometry, materials, [6,6,6,6,6,6]);
-        this.upperArmLeftMesh.position = new Vector3(-0.175, -0.15, 0);
+        this.armLeftMesh = new Mesh("Arm Left", this.#armGeometry, materials, [6,6,6,6,6,6]);
+        this.armLeftMesh.position = new Vector3(-0.175, -0.15, 0);
 
-        this.upperArmRightMesh = new Mesh("Upper Arm Right", this.#upperArmGeometry, materials, [6,6,6,6,6,6]);
-        this.upperArmRightMesh.position = new Vector3(0.175, -0.15, 0);
+        this.armRightMesh = new Mesh("Arm Right", this.#armGeometry, materials, [6,6,6,6,6,6]);
+        this.armRightMesh.position = new Vector3(0.175, -0.15, 0);
 
-        this.upperLegLeftMesh = new Mesh("Upper Leg Left", this.#upperLegGeometry, materials, [6,6,6,6,6,6]);
-        this.upperLegLeftMesh.position = new Vector3(-0.08, -0.18, 0);
+        this.legLeftMesh = new Mesh("Leg Left", this.#legGeometry, materials, [6,6,6,6,6,6]);
+        this.legLeftMesh.position = new Vector3(-0.08, -0.18, 0);
 
-        this.upperLegRightMesh = new Mesh("Upper Leg Right", this.#upperLegGeometry, materials, [6,6,6,6,6,6]);
-        this.upperLegRightMesh.position = new Vector3(0.08, -0.18, 0);
+        this.legRightMesh = new Mesh("Leg Right", this.#legGeometry, materials, [6,6,6,6,6,6]);
+        this.legRightMesh.position = new Vector3(0.08, -0.18, 0);
 
         this.hipMesh = new Mesh("hip", this.#hipGeometry, materials, [6,6,6,6,6,6]);
         this.hipMesh.position = new Vector3(0,-0.15,0);
@@ -42,9 +42,9 @@ class Golem{
 
         this.headMesh.add(this.noseMesh)
 
-        this.hipMesh.add(this.upperLegLeftMesh,this.upperLegRightMesh)
+        this.hipMesh.add(this.legLeftMesh,this.legRightMesh)
 
-        this.torsoMesh.add(this.headMesh, this.upperArmLeftMesh, this.upperArmRightMesh, this.hipMesh);
+        this.torsoMesh.add(this.headMesh, this.armLeftMesh, this.armRightMesh, this.hipMesh);
 
         this.root = new Mesh("Golem", new BoxGeometry(0, 0, 0), materials, [0, 0, 0, 0, 0, 0]);
         this.root.position = new Vector3(0,-0.2,0)
