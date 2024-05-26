ebc4aec7fbabbca80b90c1605f21dcb61060f170
Agsha Athalla Nurkareem <agshaathalla@gmail.com>
2024-05-26 22:15:01 +0700

    refactor: wither model

--- src/models/wither.js
+++ src/models/wither.js
@@ -1,64 +1,56 @@
 class Wither{
-    #neckGeometry
-    #bodyGeometry
-    #boneGeometry
-    #sideHeadGeometry
-    #centerHeadGeometry
-    #materials = []
 
     get object(){
         
-        this.#materials.push(new BasicMaterial("white", [1, 1, 1], false))
-        this.#materials.push(new BasicMaterial("grey", [0.55, 0.55, 0.55], false))
+        const materials = [new BasicMaterial("white", [1, 1, 1], false), new BasicMaterial("grey", [0.55, 0.55, 0.55], false)]
 
+        const neckGeometry = new BoxGeometry(0.25, 0.025,0.025);
+        const bodyGeometry = new BoxGeometry(0.025, 0.22, 0.025);
+        const boneGeometry = new BoxGeometry(0.05, 0.025, 0.015);
+        const sideHeadGeometry = new BoxGeometry(0.075, 0.075, 0.075);
+        const centerHeadGeometry =  new BoxGeometry(0.09, 0.09, 0.09);
 
-        this.#neckGeometry = new BoxGeometry(0.25, 0.025,0.025);
-        this.#bodyGeometry = new BoxGeometry(0.025, 0.22, 0.025);
-        this.#boneGeometry = new BoxGeometry(0.05, 0.025, 0.015);
-        this.#sideHeadGeometry = new BoxGeometry(0.075, 0.075, 0.075);
-        this.#centerHeadGeometry =  new BoxGeometry(0.09, 0.09, 0.09);
+        const neckMesh = new Mesh("Neck", neckGeometry, materials, [0, 0, 0, 0, 0, 0]);
+        neckMesh.position = new Vector3(0, 0.1, 0);
 
-        this.neckMesh = new Mesh("Neck", this.#neckGeometry, this.#materials, [0, 0, 0, 0, 0, 0]);
-        this.neckMesh.position = new Vector3(0, 0.1, 0);
+        const bodyMesh = new Mesh("Body", bodyGeometry, materials, [0, 0, 0, 0, 0, 0]);
+        bodyMesh.position = new Vector3(0, 0, 0);
 
-        this.bodyMesh = new Mesh("Body", this.#bodyGeometry, this.#materials, [0, 0, 0, 0, 0, 0]);
-        this.bodyMesh.position = new Vector3(0, 0, 0);
+        const bone1LeftMesh = new Mesh("Bone1L", boneGeometry, materials, [1, 1, 1, 1, 1, 1]);
+        bone1LeftMesh.position = new Vector3(-0.03, 0.05, 0);
 
-        this.bone1LeftMesh = new Mesh("Bone1L", this.#boneGeometry, this.#materials, [1, 1, 1, 1, 1, 1]);
-        this.bone1LeftMesh.position = new Vector3(-0.03, 0.05, 0);
+        const bone2LeftMesh = new Mesh("Bone2L", boneGeometry, materials, [1, 1, 1, 1, 1, 1]);
+        bone2LeftMesh.position = new Vector3(-0.03, 0, 0);
 
-        this.bone2LeftMesh = new Mesh("Bone2L", this.#boneGeometry, this.#materials, [1, 1, 1, 1, 1, 1]);
-        this.bone2LeftMesh.position = new Vector3(-0.03, 0, 0);
+        const bone3LeftMesh = new Mesh("Bone3L", boneGeometry, materials, [1, 1, 1, 1, 1, 1]);
+        bone3LeftMesh.position = new Vector3(-0.03, -0.05, 0);
 
-        this.bone3LeftMesh = new Mesh("Bone3L", this.#boneGeometry, this.#materials, [1, 1, 1, 1, 1, 1]);
-        this.bone3LeftMesh.position = new Vector3(-0.03, -0.05, 0);
+        const bone1RightMesh = new Mesh("Bone1R", boneGeometry, materials, [1, 1, 1, 1, 1, 1]);
+        bone1RightMesh.position = new Vector3(0.03, 0.05, 0);
 
-        this.bone1RightMesh = new Mesh("Bone1R", this.#boneGeometry, this.#materials, [1, 1, 1, 1, 1, 1]);
-        this.bone1RightMesh.position = new Vector3(0.03, 0.05, 0);
+        const bone2RightMesh = new Mesh("Bone2R", boneGeometry, materials, [1, 1, 1, 1, 1, 1]);
+        bone2RightMesh.position = new Vector3(0.03, 0, 0);
 
-        this.bone2RightMesh = new Mesh("Bone2R", this.#boneGeometry, this.#materials, [1, 1, 1, 1, 1, 1]);
-        this.bone2RightMesh.position = new Vector3(0.03, 0, 0);
+        const bone3RightMesh = new Mesh("Bone3R", boneGeometry, materials, [1, 1, 1, 1, 1, 1]);
+        bone3RightMesh.position = new Vector3(0.03, -0.05, 0);
 
-        this.bone3RightMesh = new Mesh("Bone3R", this.#boneGeometry, this.#materials, [1, 1, 1, 1, 1, 1]);
-        this.bone3RightMesh.position = new Vector3(0.03, -0.05, 0);
+        const headLeftMesh = new Mesh("HeadL", sideHeadGeometry, materials, [0, 0, 0, 0, 0, 0]);
+        headLeftMesh.position = new Vector3(-0.1, 0.025, 0.025);
 
-        this.headLeftMesh = new Mesh("HeadL", this.#sideHeadGeometry, this.#materials, [0, 0, 0, 0, 0, 0]);
-        this.headLeftMesh.position = new Vector3(-0.1, 0.025, 0.025);
+        const headRightMesh = new Mesh("HeadR", sideHeadGeometry, materials, [0, 0, 0, 0, 0, 0]);
+        headRightMesh.position = new Vector3(0.1, 0.025, 0.025);
 
-        this.headRightMesh = new Mesh("HeadR", this.#sideHeadGeometry, this.#materials, [0, 0, 0, 0, 0, 0]);
-        this.headRightMesh.position = new Vector3(0.1, 0.025, 0.025);
-
-        this.headCenterMesh = new Mesh("HeadC", this.#centerHeadGeometry, this.#materials, [0, 0, 0, 0, 0, 0]);
-        this.headCenterMesh.position = new Vector3(0, 0.04, 0);
+        const headCenterMesh = new Mesh("HeadC", centerHeadGeometry, materials, [0, 0, 0, 0, 0, 0]);
+        headCenterMesh.position = new Vector3(0, 0.04, 0);
 
         // Assemble parts
-        this.neckMesh.add(this.headCenterMesh, this.headLeftMesh, this.headRightMesh);
-        this.bodyMesh.add(this.neckMesh, this.bone1LeftMesh, this.bone2LeftMesh, this.bone3LeftMesh, this.bone1RightMesh, this.bone2RightMesh, this.bone3RightMesh);
+        neckMesh.add(headCenterMesh, headLeftMesh, headRightMesh);
+        bodyMesh.add(neckMesh, bone1LeftMesh, bone2LeftMesh, bone3LeftMesh, bone1RightMesh, bone2RightMesh, bone3RightMesh);
 
-        this.root = new Mesh("Wither", new BoxGeometry(0, 0, 0), this.#materials, [0, 0, 0, 0, 0, 0]);
-        this.root.position = new Vector3(0, 0, 0);
-        this.root.add(this.bodyMesh);
+        const root = new Mesh("Wither", new BoxGeometry(0, 0, 0), materials, [0, 0, 0, 0, 0, 0]);
+        root.position = new Vector3(0, 0, 0);
+        root.add(bodyMesh);
 
-        return this.root;
+        return root;
     }
 }
